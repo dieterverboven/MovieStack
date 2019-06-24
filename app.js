d1e85bdfd6a8477f1a10a95a14fe344957a8766c
dieterverboven <d.verboven@theoryx.com>
2019-06-24 13:06:18 +0200

    app.js changes

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use('/Food', foodRoutes);
 
 app.get('/', (req, res)=> {
     res.send('Hello World');
-});
+ });
  
-app.listen(process.env.PORT || 3000, function(){
-     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+ app.listen(3000, ()=>{
+     console.log("Server is running");
+ });
